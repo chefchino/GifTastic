1d459a182bae49d22076a11d809359cf36cf3d88
Ciprien Bossut <chefchino@live.com>
2019-10-17 17:53:02 -0400

    hw1

--- javascript/test.js
+++ javascript/test.js
@@ -1,23 +1,14 @@
-document.cookie = 'cross-site-cookie=bar; SameSite=None; Secure';
-console.log("working");
 var topics= ["waterfalls", "mountains", "beaches", "islands","trees", "bears", "eagles"];
-
-
 function displayTopicGiphy(){
     $("#topic-view").prepend();
     var topic = $(this).attr("data-name");
     var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=N70F0bUVCX3gByJTgX7dA0k2SUFY4LEK&limit=10&q="+ topic;
-    https://cors-anywhere.herokuapp.com/
     $.ajax({
         url: queryURL,
         method: "GET"
     }).done(function(response){
         var results = response.data;
-
         for (var i = 0; i < results.length; i++){
-
-
-        
         var topicDiv = $("<div class='flex'>");
         var title= results[i].title;
         var rating= results[i].rating;
@@ -31,19 +22,12 @@ function displayTopicGiphy(){
         image.attr("data-still", stillImgURL);
         image.attr("data-animate",animateImgURL);
         image.attr("crossorgin", "anonymous");
-
         topicDiv.append(image);
         $("#topic-view").prepend(topicDiv);
-        console.log("response",response);
-    
     }
-
     $(".topic1").on('click', function(){
 
         var state = $(this).attr("data-state");
-
- console.log(this);
-
     if (state === "still") {
         $(this).attr("src", $(this).attr("data-animate"));
         $(this).attr("data-state", "animate");
@@ -53,8 +37,6 @@ function displayTopicGiphy(){
       }})
 })
 }
-
-
 function renderButtons(){
 
     $("#buttons").empty();
@@ -75,28 +57,3 @@ renderButtons();
 
 $(document).on("click", ".topicBtn", displayTopicGiphy);
 renderButtons();
-           // for (var i = 0; i < results.length; i++) {
-
-        //     if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
-          
-        //       var gifDiv = $("<div>");
-
-
-        //       var rating = results[i].rating;
-
-
-        //       var p = $("<p>").text("Rating: " + rating);
-
-
-        //       var personImage = $("<img>");
-
-     
-        //       personImage.attr("src", results[i].images.fixed_height.url);
-
-        //       gifDiv.append(p);
-        //       gifDiv.append(personImage);
-
-
-        //       $("#topic-view").prepend(gifDiv);
-        //     }}});
-        // }
